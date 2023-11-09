c3e07b7e2667b06e6c116686773167de3691458a
Smruti-Rani <91662340+Smrutirani@users.noreply.github.com>
2023-11-09 11:39:49 +0000

    test is changed

---
+++ test.js
@@ -0,0 +1,12 @@
+// console.log("Smruti")
+const p = {
+    firstName: "Smruti",
+    age: 25
+}
+firstName = "Arun"
+let lastName = "Rani"
+ lastName = "Panigrahy"
+console.log(p.firstName);
+ console.log(lastName);
+ console.log(typeof null);
+ console.log(typeof undefined);
